e6a34443cea9eddd9bda275ec7851b37fa1fddf6
Anthony Simpson <ansi6622@colorado.edu>
2016-03-23 22:01:19 -0600

    maybe if i spell my name right itll work...

--- create_url_shortener_database.sql
+++ create_url_shortener_database.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS url_shortener_anthony_simpson;
 DROP TABLE IF EXISTS urls;
-CREATE DATABASE url_shortener_anthony_aimpson;
+CREATE DATABASE url_shortener_anthony_simpson;
 \c url_shortener_anthony_simpson;
 
 CREATE TABLE urls (
